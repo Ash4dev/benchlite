d1d45f067f305faaf257144b6db053b0bf074f22
Ashutosh Panigrahy <ashutoshp.iitkgp@gmail.com>
2026-05-21 13:39:17 +0530

    framework in action

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main.cpp
+ * Author: Ashutosh Panigrahy
+ * Created: 2026-05-19
+ * Description: 
+ * NOTE: keep it working & simple
+ */
+
+#include "expts/growth.hpp"
+
+int main(){
+  run_vector_growth_experiment("run/growth_latest.csv");
+  return 0;
+}
